83f6d9ec8b6bf9384ae411dc59ad9d90cebac441
GezuzRome0 <gezuzromeo2@gmail.com>
2026-03-18 20:25:21 -0500

    Agregando clase repository

--- src/main/java/com/ProductService/Product/Repository/ProductRepository.java
+++ src/main/java/com/ProductService/Product/Repository/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.ProductService.Product.Repository;
 
 import com.ProductService.Product.Entity.ProductEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
+    @Modifying
+    @Transactional
+    @Query(value = "INTO sh_venta.tb_product (name_product, description_product, price_unid_product)" +
+    "VALUES (:name_product, :description_product, :price_unid_product);",nativeQuery = true)
+    void addProduct(@Param("name_product")String nameProduct,
+                    @Param("description_product")String descriptionProduct,
+                    @Param("price_unid_product")String priceUnidProduct);
 }
